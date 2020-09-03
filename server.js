2a1330818609e2551af0f0ed7e45472b79c58666
y0092888 <ju-horn@web.de>
2020-09-03 15:47:22 +0200

    server serves client

--- server.js
+++ server.js
@@ -5,12 +5,17 @@ const cors = require("cors");
 const stuff = require("./api/stuff");
 const videos = require("./api/videos");
 const errorHandler = require("./middleware/error");
+
+const path = require("path");
+
 const app = express();
 
 app.use(cors());
 app.use(express.json());
 app.use("/stuff", stuff);
 app.use("/videos", videos);
+
+app.use(express.static(path.join(__dirname, "client", "build")));
 app.use(errorHandler);
 
-app.listen("4000", () => console.log("connected"));
+app.listen(process.env.PORT || "3001", () => console.log("connected"));
